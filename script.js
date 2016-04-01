639cb31f4f40f87bc7866c6bb7a734a89f9696a6
stephenbienkiewicz <bienkiewicz@outlook.com>
2016-03-31 22:43:13 -0400

    final

--- script.js
+++ script.js
@@ -55,52 +55,52 @@ $(document).ready(function() {
 
   function checkTime(i) {
       if (i < 10) {
-      	i = "0" + i
+        i = "0" + i
       };  // add zero in front of numbers < 10
       
       return i;
   }
 
   function changeColor(i) {
-    if (i % 2 === 1) { // odd
-      $("body").css("color", "blue",);
-    } else { // even
+    if (i % 2 === 1) { 
+      $("body").css("color", "blue");
+    } else { 
       $("body").css("color", "red");
     }
   }
 
   //-------------------------------------------------------------------
 
-  function changeBackgroundColor() {
-    var currentBackgroundColor = $("body").css("backgroundColor");
+  function changeBackgroundColor() { // function is created. the function alternates between two background colors.
+    var currentBackgroundColor = $("body").css("backgroundColor"); // declare currentBackgroundColor. 
 
-    if (currentBackgroundColor === "rgb(255, 127, 80)") {
-      $("body").css("backgroundColor", "lime");
-    } else {
-      $("body").css("backgroundColor", "coral");
+    if (currentBackgroundColor === "rgb(255, 127, 80)") { // if statement. currentBackgroundColor is set to coral hex code.
+      $("body").css("backgroundColor", "lime"); // html body is set to lime
+    } else { // else statement.
+      $("body").css("backgroundColor", "coral"); // html body is set to coral. //
     }
 
-    setTimeout(changeBackgroundColor, 5000); // should be 5 minutes, but this is easier to test
+    setTimeout(changeBackgroundColor, 5000); // time is set for changeBackgroundColor.
   }
 
-  var counter = 0;
+  var counter = 0; // counter is set to 0.
 
-  function changeFontSize() {
-    var fontSizes = ["60px", "90px", "10px", "20px"];
+  function changeFontSize() { // function is created. the function alternates between multiple font sizes.
+    var fontSizes = ["60px", "90px", "10px", "20px"]; // declare fontSizes.
 
-    $("body").css("fontSize", fontSizes[counter]);
+    $("body").css("fontSize", fontSizes[counter]); //
 
-    counter = counter + 1;
-    if (counter > 3) {
-      counter = 0;
+    counter = counter + 1; // counter formula equals counter (0) + 1.
+    if (counter > 3) { // if counter is greater than 3.
+      counter = 0; // counter is reset to 0.
     }
     
-    setTimeout(changeFontSize, 5000); // should be 1 hour, but this is easier to test
+    setTimeout(changeFontSize, 5000); // time is set for changeFontSize.
   }
 
-  startTime(); // function is called for startTime
-  changeBackgroundColor(); // function is called for changeBackgroundColor
-  changeFontSize(); // function is called for changeFontSize
+  startTime(); // function is called for startTime.
+  changeBackgroundColor(); // function is called for changeBackgroundColor.
+  changeFontSize(); // function is called for changeFontSize.
 });
 
 
